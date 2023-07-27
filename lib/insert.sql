f3b3ca77ce71b8204cf1fea194dfc367562c075f
Giselle <gisellec60@gmail.com>
2023-07-27 08:37:29 -0400

    passed all first time

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES ("Mr. Chocolate",3,"M","brown","friendly",1);
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES ("Rowdy",1,"M","gray","aggressive",0);
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES ("Tabitha",13,"F","black","social",1);
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES ("Sergeant Brown",9,"M","white","excitable",0);
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES ("Melissa",5,"F","tan","hypersensitve",1);
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES ("Grinch",11,"M","brown","moody",0);
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES ("Wendy",4,"F","black","excitable",1);
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES (NULL,13,"F","tan","calm",1);
